8b0cefa6b30045e80eb4c14b95922810ee36a127
hkzs123 <947653555@qq.com>
2019-02-17 00:40:09 +0800

    change

--- app/src/main/java/com/example/sqlitedemo/MainActivity.java
+++ app/src/main/java/com/example/sqlitedemo/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.sqlitedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text1 = findViewById(R.id.text1);
+        text1.setText("hahhahhha");
+
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "click", Toast.LENGTH_SHORT);
+            }
+        });
+
     }
 }
